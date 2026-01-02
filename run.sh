f2026ed2038d2cf2ad4a5dcc6521ef8e0e0f2b07
asemenov <alexandr.v.semenov@nasa.gov>
2026-01-02 14:58:06 -0500

    fixed formatting

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 echo "C++ with no SIMD exponent"
 build/testBilateral_cpp_no_simd 10000 5 0.5;
-echo "\nC++ with SIMD exponent\n"
+echo "\nC++ with SIMD exponent"
 build/testBilateral_cpp 10000 5 0.5;
-echo "\nFortran\n"
+echo "\nFortran"
 build/testBilateral_fortran 10000 5 0.5
-echo "\nRust\n"
+echo "\nRust"
 build/testBilateral_rust 10000 5 0.5
