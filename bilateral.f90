aa6864d6617614feebaff4ea4c4d8f84b10d6796
asemenov <alexandr.v.semenov@nasa.gov>
2026-01-02 14:55:01 -0500

    fixed a bug

--- bilateral.f90
+++ bilateral.f90
@@ -36,7 +36,7 @@ program main
                 call str2int(trim(arg_value), N, status)
             end if
             if(i == 2) then
-                call str2real(trim(arg_value), radius, status)
+                call str2int(trim(arg_value), radius, status)
             end if
             if(i == 3) then
                 call str2real(trim(arg_value), sigma, status)
